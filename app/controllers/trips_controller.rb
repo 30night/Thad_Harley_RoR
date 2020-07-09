7bf4a5f292c488738b1236265e9377499358b8fa
30night <haroscmil2011@gmail.com>
2020-07-09 16:24:15 +1000

    rails backend

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,37 @@
+class TripsController < ApplicationController
+  before_action :set_trip, only: [:show, :update, :destroy]
+
+  def index 
+    @trips = Trip.all.order(id: "desc")
+    render json: @trips
+  end 
+
+  def show 
+    render json: @trip
+  end 
+
+  def create 
+    Trip.create(trip_params)
+    render json: "Trip suggestion created", status: 200 
+  end 
+
+  def update 
+    @trip.update(trip_params)
+    render json: "Suggestion updated", status: 200
+  end 
+
+  def destroy
+    @trip.destroy
+    render json: "Suggestion deleted", status: 200
+  end 
+
+  private 
+
+  def trip_params 
+    params.require(:trip).permit(:title, :description, :name)
+  end 
+
+  def set_trip 
+    @trip = Trip.find(params[:id])
+  end 
+end
---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Trip.create(title: 'japan', name: 'harley', description: 'hi my name is harley')
+Trip.create(title: 'korea', name: 'james', description: 'very cool')
+Trip.create(title: 'bali', name: 'Thad', description: 'wow')
+Trip.create(title: 'america', name: 'bob', description: 'im fat')
